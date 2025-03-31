16acfaf4821fa62dffc021ca6238b8d736831bb7
tttol <tkhstol.929@gmail.com>
2025-04-01 05:59:13 +0900

    add CloudFront

--- lib/about-me-infra-stack.ts
+++ lib/about-me-infra-stack.ts
@@ -1,27 +1,63 @@
 import * as cdk from 'aws-cdk-lib';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
+
+// export interface AboutMeInfraStackProps extends cdk.StackProps {
+//   // No properties needed when not using custom domain
+// }
 
 export class AboutMeInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // S3 bucket for static assets
     const s3Bucket = new s3.Bucket(this, 'AboutMeInfraBucket', {
-      bucketName: 'about-me-infra',
-      publicReadAccess: true,
-
+      bucketName: `about-me-infra-${this.account}`,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       encryption: s3.BucketEncryption.S3_MANAGED,
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
     });
 
+    // CloudFront distribution - HTTPS is provided by default
+    const distribution = new cloudfront.Distribution(this, 'AboutMeDistribution', {
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(s3Bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        compress: true,
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+    });
+    
     // To deploy assets to s3 bucket
     const s3Deployment = new s3deploy.BucketDeployment(this, 'AboutMeInfraDeployment', {
       sources: [s3deploy.Source.asset('./asset')],
       destinationBucket: s3Bucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+    
+    // Output the CloudFront URL
+    new cdk.CfnOutput(this, 'CloudFrontURL', {
+      value: `https://${distribution.distributionDomainName}`,
+      description: 'CloudFront Distribution URL',
     });
   }
 }
