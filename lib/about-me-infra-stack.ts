148223c7b04a9ee79d30a1a54444195517b2a8dc
tttol <tkhstol.929@gmail.com>
2025-03-31 21:31:44 +0900

    enable static web site hosting

--- lib/about-me-infra-stack.ts
+++ lib/about-me-infra-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
 export class AboutMeInfraStack extends cdk.Stack {
@@ -8,17 +9,19 @@ export class AboutMeInfraStack extends cdk.Stack {
 
     const s3Bucket = new s3.Bucket(this, 'AboutMeInfraBucket', {
       bucketName: 'about-me-infra',
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       encryption: s3.BucketEncryption.S3_MANAGED,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
     });
 
     // To deploy assets to s3 bucket
-    // const s3Deployment = new s3deploy.BucketDeployment(this, 'AboutMeInfraDeployment', {
-    //   sources: [s3deploy.Source.asset('../frontend/dist')],
-    //   destinationBucket: s3Bucket,
-    // });
+    const s3Deployment = new s3deploy.BucketDeployment(this, 'AboutMeInfraDeployment', {
+      sources: [s3deploy.Source.asset('./asset')],
+      destinationBucket: s3Bucket,
+    });
   }
 }
