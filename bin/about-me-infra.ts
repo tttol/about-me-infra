b2d9deffd158d2d49a8a50c878fd23b758e4323f
tttol <tkhstol.929@gmail.com>
2025-04-10 06:23:23 +0900

    add ACM, Route53

--- bin/about-me-infra.ts
+++ bin/about-me-infra.ts
@@ -8,4 +8,6 @@ import { AboutMeInfraStack } from '../lib/about-me-infra-stack';
 const app = new cdk.App();
 new AboutMeInfraStack(app, 'AboutMeInfraStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  domainName: process.env.DOMAIN_NAME,
+  hostedZoneId: process.env.HOSTED_ZONE_ID,
 });
--- lib/about-me-infra-stack.ts
+++ lib/about-me-infra-stack.ts
@@ -1,17 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
-// export interface AboutMeInfraStackProps extends cdk.StackProps {
-//   // No properties needed when not using custom domain
-// }
+export interface AboutMeInfraStackProps extends cdk.StackProps {
+  domainName: string | undefined;
+  hostedZoneId: string | undefined;
+}
 
 export class AboutMeInfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: AboutMeInfraStackProps) {
     super(scope, id, props);
+
+    // Validate required parameters
+    if (!props.domainName) {
+      throw new Error('domainName is required');
+    }
+    if (!props.hostedZoneId) {
+      throw new Error('hostedZoneId is required');
+    }
+
+    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+      hostedZoneId: props.hostedZoneId,
+      zoneName: props.domainName,
+    });
 
     // S3 bucket for static assets
     const s3Bucket = new s3.Bucket(this, 'AboutMeInfraBucket', {
@@ -23,7 +39,15 @@ export class AboutMeInfraStack extends cdk.Stack {
       encryption: s3.BucketEncryption.S3_MANAGED,
     });
 
-    // CloudFront distribution - HTTPS is provided by default
+    // Create ACM certificate in us-east-1 for CloudFront
+    // const certificate = new acm.Certificate(this, 'Certificate', {
+    //   domainName: props.domainName,
+    // });
+
+    const certificateArn = process.env.ACM_CERTIFICATE_ARN || '';
+    const certificate = acm.Certificate.fromCertificateArn(this, 'Certificate', certificateArn);
+
+    // CloudFront distribution with custom domain
     const distribution = new cloudfront.Distribution(this, 'AboutMeDistribution', {
       defaultBehavior: {
         origin: origins.S3BucketOrigin.withOriginAccessControl(s3Bucket),
@@ -31,6 +55,8 @@ export class AboutMeInfraStack extends cdk.Stack {
         cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
         compress: true,
       },
+      domainNames: [props.domainName],
+      certificate: certificate,
       defaultRootObject: 'index.html',
       errorResponses: [
         {
@@ -45,19 +71,19 @@ export class AboutMeInfraStack extends cdk.Stack {
         },
       ],
     });
-    
-    // To deploy assets to s3 bucket
-    const s3Deployment = new s3deploy.BucketDeployment(this, 'AboutMeInfraDeployment', {
-      sources: [s3deploy.Source.asset('./asset')],
-      destinationBucket: s3Bucket,
-      distribution,
-      distributionPaths: ['/*'],
+
+    // Create A record for the domain
+    new route53.ARecord(this, 'AliasRecord', {
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(
+        new targets.CloudFrontTarget(distribution)
+      ),
     });
     
     // Output the CloudFront URL
     new cdk.CfnOutput(this, 'CloudFrontURL', {
-      value: `https://${distribution.distributionDomainName}`,
-      description: 'CloudFront Distribution URL',
+      value: `https://${props.domainName}`,
+      description: 'Custom Domain URL',
     });
   }
 }
